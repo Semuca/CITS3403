dc37a19bf3e1eb33fb25d81e997081f1a095dff4
Semuca <pokelock@icloud.com>
2024-03-25 12:16:56 +0800

    Fixing pylint errors for the initial main.py file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""This module is the entry point for the flask application"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return f"<p>Hello, World!</p>"
+    """A test 'hello world' function"""
+    return "<p>Hello, World!</p>"
