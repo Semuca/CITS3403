8cf162772dbe632322fe013db085275da497b085
Jared Healy <healy_jared@yahoo.com>
2024-03-26 02:43:21 +0800

    Add Bare bones Login page frontend and dummy backend

---
+++ static/login.js
@@ -0,0 +1,55 @@
+function hash(str){
+    /**
+     * COPIED DIRECTLY FROM https://stackoverflow.com/a/26057776
+     * TODO: Maybe we should rewrite this ourselves? not sure...
+     */
+    let hash = 0;
+    if (str.length === 0) return hash;
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+
+jQuery(() => {
+    $("#pressLogin").click(() => {
+        fetch("/api/login", {
+            method: "POST", body: JSON.stringify({
+                username: $("#username").val(),
+                password: hash($("#password").val())
+            }), headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(r => {
+            if (r.ok) {
+                r.json().then(
+                    o => {
+                        const expiry = new Date(Date.now() + (25 * 60 * 60)) //set the cookie to expire in 24 hours
+                        document.cookie += `token=${o.token}; expiry=${expiry}; path=/`
+                        document.location = "/"
+                    }
+                )
+            } else {
+                alert(`The server did not return a valid response! HTTP error code is ${r.status} (${r.statusText})`)
+            }
+        })
+
+    })
+
+    $("#pressCreate").click(() => {
+        fetch("/api/create_account", {
+            method: "POST", body: JSON.stringify({
+                username: $("#username").val(),
+                password: hash($("#password").val())
+            }), headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(r => {
+           location.reload()
+        })
+
+    })
+})
