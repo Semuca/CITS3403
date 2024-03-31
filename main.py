8d922dfeea6809c202d943051cb95ccc4fd7d181
Semuca <pokelock@icloud.com>
2024-03-31 13:03:25 +0800

    Removing temporary log in template

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """This module is the entry point for the flask application"""
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -15,10 +15,3 @@ def hello_world():
     """A test 'hello world' function"""
 
     return "<p>Hello, World!</p>"
-
-@app.route("/login")
-def login_page():
-    """The login page"""
-
-    user = {'username': 'Miguel'}
-    return render_template('login.html', title='Home', user=user)
