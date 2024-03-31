50b1284753a243229c71b588fe4a091a3c411c1a
Semuca <pokelock@icloud.com>
2024-03-31 13:04:02 +0800

    Renaming the blueprint api, fixing some typos and commenting endpoints

--- api/login.py
+++ api/login.py
@@ -11,27 +11,32 @@ from common import validation
 def create_user():
     """Endpoint to register a user"""
 
+    # Get validated data
     data = validation.request_body_from_schema(createUserBodySchema)
     if isinstance(data, type("")):
         return make_response({"error": "Request validation error", "errorMessage": data}, 400)
 
-
+    # Open connection to db
     db = sqlite3.connect("databases/database.db")
     cursor = db.cursor()
 
-    token = secrets.token_urlsafe()
-
+    # Find if a user by that username already exists
     res = cursor.execute(f"SELECT username FROM users WHERE username='{data["username"]}'")
 
     if (len(res.fetchall()) != 0):
         return make_response({"error": "Request validation error", "errorMessage": "User already exists"}, 403)
 
+    # Create token
+    token = secrets.token_urlsafe()
 
+    # Add the user into the database
     cursor.execute(f"INSERT INTO users VALUES ('{data["username"]}', '{data["password"]}', '{token}')")
 
+    # Commit the database and close
     db.commit()
     db.close()
 
+    # Return with the token
     return make_response({"token": token})
 
 createUserBodySchema = {
@@ -43,23 +48,29 @@ createUserBodySchema = {
 def login():
     """Endpoint to let a user log in"""
 
+    # Get validated data
     data = validation.request_body_from_schema(loginBodySchema)
     if isinstance(data, type("")):
         return make_response({"error": "Request validation error", "errorMessage": data}, 400)
 
+    # Open connection to db
     # TODO (James): Constants for getting the database file
     db = sqlite3.connect("databases/database.db")
     cursor = db.cursor()
 
+    # Find a user by that username and passwordHash
     res = cursor.execute(f"SELECT username FROM users WHERE username='{data["username"]}' AND passwordHash='{data["password"]}'")
 
-    if (len(res.fetchall()) == 0):
+    if len(res.fetchall()) == 0:
         return make_response({"error": "Request validation error", "errorMessage": "User not found"}, 404)
 
+    # Create token
     token = secrets.token_urlsafe()
 
+    # Update the token against that user
     cursor.execute(f"UPDATE users SET accessToken='{token}' WHERE username='{data["username"]}' AND passwordHash='{data["password"]}'")
 
+    # Return with the token
     return make_response({"token": token})
 
 loginBodySchema = {
