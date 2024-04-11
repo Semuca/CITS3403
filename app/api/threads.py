059135bd2634bce0e5c845462f76da93904bb601
Semuca <pokelock@icloud.com>
2024-04-11 21:03:54 +0800

    #13 adds authentication helper function and attaches it to threads endpoints

--- app/api/threads.py
+++ app/api/threads.py
@@ -4,22 +4,31 @@ from flask import make_response
 
 from app.databases import db
 from app.models import ThreadModel
-from app.helpers.json_schema_validation import validate_request_schema
+from app.helpers import get_user_by_token, validate_request_schema
 
 from .bp import api_bp
 
 @api_bp.route('/threads', methods=['POST'])
 def create_thread():
     """Creates a thread object and saves it to the database"""
+
     # Validate post request schema
     data = validate_request_schema(create_thread_schema)
     if isinstance(data, str):
         return make_response({"error": "Request validation error", "errorMessage": data}, 400)
 
+    # Authorize request
+    request_user_id = get_user_by_token()
+
+    if request_user_id is None:
+        return make_response({"error": "Authorization error",
+                              "errorMessage": "Authorization token not valid"},
+                             401)
+
     # Create a thread object from parameters passed in
     new_thread = ThreadModel(title=data['title'],
         description=data['description'],
-        user_id=data['userId'])
+        user_id=request_user_id)
 
     # Save to db
     db.session.add(new_thread)
@@ -30,13 +39,20 @@ def create_thread():
 
 create_thread_schema = {
     "title": "string",
-    "description": "string",
-    "userId": "int"
+    "description": "string"
 }
 
 @api_bp.route('/threads', methods=['GET'])
 def read_many_thread():
     """Reads a list thread objects from the database based on query parameters"""
+
+    # Authorize request
+    request_user_id = get_user_by_token()
+
+    if request_user_id is None:
+        return make_response({"error": "Authorization error",
+                              "errorMessage": "Authorization token not valid"},
+                             401)
 
     # Get a list of thread objects according to parameters
     queried_threads = ThreadModel.query.all() # to-do sorting and filtering later
---
+++ app/helpers/authenticate_token.py
@@ -0,0 +1,23 @@
+"""This module provides authentication helper functions for api endpoints"""
+
+from flask import request
+from app.models import UserModel
+
+def get_user_by_token():
+    """Gets a user id by the token
+    Returns:
+        A user id if the token is stored against a user
+        None otherwise
+    """
+
+    # Expected value of Authorization header: 'Bearer <token>'
+    authorization_header = request.headers.get('Authorization').split(' ')
+
+    if len(authorization_header) != 2:
+        return None
+
+    token = authorization_header[1]
+
+    res = UserModel.query.filter_by(authentication_token=token).first()
+
+    return res.id if res is not None else None
