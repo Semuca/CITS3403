691106adfc18201ed59569b04cee3ee7ce93f452
Jared Healy <healy_jared@yahoo.com>
2024-04-15 11:56:18 +0800

    Set the thread details up to be sent statically instead of queried from the client-side

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,10 @@
 from flask import Blueprint, render_template
 
 from app.helpers import redirect_wrapper
+from app.models import CommentModel
 
 main_bp = Blueprint('main_bp', __name__)
+
 
 @main_bp.route("/")
 def home_page():
@@ -12,11 +14,13 @@ def home_page():
 
     return render_template('home.html')
 
+
 @main_bp.route("/login")
 def login_page():
     """The login page"""
 
     return render_template('login.html')
+
 
 @main_bp.route("/register")
 def signup_page():
@@ -24,14 +28,24 @@ def signup_page():
 
     return render_template('register.html')
 
+
 @main_bp.route("/forum")
 def forum_page():
     """The trade forum page"""
 
     return redirect_wrapper(render_template('forum.html'))
 
+
 @main_bp.route("/thread/<int:thread_id>")
 def thread_page(thread_id):
     """The single thread page"""
-
-    return redirect_wrapper(render_template('thread.html', thread_id=thread_id))
+    # TODO (Jared): Find a way to get this information without breaking the space time continuim
+    from app import db, ThreadModel
+    thread = db.session.get(ThreadModel, thread_id)
+    comments = [i.comment_text for i in db.session.scalars(
+        db.select(CommentModel)
+        .where(CommentModel.thread_id == thread_id)
+        .order_by(CommentModel.created_at.asc())
+    ).all()]
+    thread.comments = comments
+    return redirect_wrapper(render_template('thread.html', thread_id=thread_id, thread=thread))
