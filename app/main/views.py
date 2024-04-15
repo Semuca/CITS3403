3a7f5eb7d7e5543418972bb2eba49c28114e1ed1
Jared Healy <healy_jared@yahoo.com>
2024-04-15 11:59:22 +0800

    Revert "Add framework for a threads view, split thread overview from create thread page."
    This reverts commit d53f6fa463f9fc070de4923894e8b4359eb741f9.

--- app/main/views.py
+++ app/main/views.py
@@ -6,13 +6,11 @@ from app.helpers import redirect_wrapper
 
 main_bp = Blueprint('main_bp', __name__)
 
-
 @main_bp.route("/")
 def hello_world():
     """A test 'hello world' function"""
 
     return "<p>Hello, World!</p>"
-
 
 @main_bp.route("/login")
 def login_page():
@@ -20,27 +18,17 @@ def login_page():
 
     return render_template('login.html')
 
-
 @main_bp.route("/register")
 def signup_page():
     """The sign up page"""
 
     return render_template('register.html')
 
-
-@main_bp.route("/forum/create")
-def create_thread_page():
-    """The trade forum page"""
-
-    return redirect_wrapper(render_template('forum.html'))
-
-
 @main_bp.route("/forum")
 def forum_page():
     """The trade forum page"""
 
-    return redirect_wrapper(render_template('view_all_threads.html'))
-
+    return redirect_wrapper(render_template('forum.html'))
 
 @main_bp.route("/thread/<int:thread_id>")
 def thread_page(thread_id):
--- app/static/forum.js
+++ app/static/forum.js
@@ -23,5 +23,24 @@ $(document).ready(() => {
         }
     })
 
-
+    $("#submit").on("click", () => {
+        fetch("/api/threads", {
+            method: "POST", headers: {
+                Authorization: `Bearer ${CookieManager.getCookie("token")}`,
+                "Content-type": "application/json; charset=UTF-8"
+            }, body: JSON.stringify({
+                title: $("#title").val(),
+                description: $("#description").val(),
+            })
+        }).then(r => {
+            if (r.ok) {
+                r.json().then(o => {
+                    threads.push(o);
+                    $("#threads").append(`<li><a href="/thread/${o.id}">${o.title}</a></li>`);
+                });
+            } else {
+                alert(`The server did not return a valid response! HTTP error code is ${r.status} (${r.statusText})`)
+            }
+        })
+    });
 });
