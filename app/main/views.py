4d338e557b8eeb2e7b7f5de2efabe53460b3c065
Jared Healy <healy_jared@yahoo.com>
2024-05-02 13:21:28 +0800

    Implement usernames for Threads page and comments

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from random import Random
 from flask import Blueprint, render_template
 
 from app.helpers import redirect_wrapper
-from app.helpers.DatabaseManager import get_comments_by_thread_id, get_thread_by_id
+from app.helpers.DatabaseManager import get_comments_by_thread_id, get_thread_by_id, get_user_by_id
 from app.models import CommentModel
 
 main_bp = Blueprint('main_bp', __name__)
@@ -42,10 +42,9 @@ def forum_page():
 def thread_page(thread_id):
     """The single thread page"""
     thread = get_thread_by_id(thread_id)
+    thread.poster = get_user_by_id(thread.user_id)
     comments = get_comments_by_thread_id(thread_id)
     # TODO (JARED): Fix this so it truly chooses your username
-    rand = Random()
     for i in comments:
-        i.username = rand.choice(["aeoniaa", "sellsol", "Semuca", "Jh1236", "fakename863", "b4d_at_c0d1ng"])
-    comments = comments
+        i.user = get_user_by_id(i.id)
     return redirect_wrapper(render_template('thread.html', thread_id=thread_id, thread=thread, comments=comments))
