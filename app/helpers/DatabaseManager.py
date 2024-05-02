e9ff6764f42d3f756c706dabf69074b525e3901d
Jared Healy <healy_jared@yahoo.com>
2024-05-02 13:21:28 +0800

    Implement usernames for Threads page and comments

--- app/helpers/DatabaseManager.py
+++ app/helpers/DatabaseManager.py
@@ -15,3 +15,12 @@ def get_thread_by_id(thread_id):
     from app import db, ThreadModel
     thread = db.session.get(ThreadModel, thread_id)
     return thread
+
+
+def get_user_by_id(user_id):
+    from app import db, UserModel
+    user = db.session.scalars(
+        db.select(UserModel)
+        .where(UserModel.id == user_id)
+    ).first()
+    return user
