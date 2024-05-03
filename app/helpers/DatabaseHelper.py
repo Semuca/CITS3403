b7852c832531ef8865aea88811877368639fca9d
Jared Healy <healy_jared@yahoo.com>
2024-05-03 10:59:52 +0800

    Add username display to the profile page

--- app/helpers/DatabaseHelper.py
+++ app/helpers/DatabaseHelper.py
@@ -5,3 +5,11 @@ def get_posts_from_user(user_id: str):
         .where(ThreadModel.user_id == user_id)
         .order_by(ThreadModel.created_at.asc())
     ).all()
+
+def get_user_by_id(user_id):
+    from app import db, UserModel
+    user = db.session.scalars(
+        db.select(UserModel)
+        .where(UserModel.id == user_id)
+    ).first()
+    return user
--- app/main/views.py
+++ app/main/views.py
@@ -3,9 +3,10 @@
 from flask import Blueprint, render_template, request
 
 from app.helpers import redirect_wrapper, get_user_id_by_auth_header, get_user_id_by_token
-from app.helpers.DatabaseHelper import get_posts_from_user
+from app.helpers.DatabaseHelper import get_posts_from_user, get_user_by_id
 
 main_bp = Blueprint('main_bp', __name__)
+
 
 @main_bp.route("/")
 def home_page():
@@ -13,11 +14,13 @@ def home_page():
 
     return render_template('home.html')
 
+
 @main_bp.route("/login")
 def login_page():
     """The login page"""
 
     return render_template('login.html')
+
 
 @main_bp.route("/register")
 def signup_page():
@@ -25,11 +28,13 @@ def signup_page():
 
     return render_template('register.html')
 
+
 @main_bp.route("/forum")
 def forum_page():
     """The trade forum page"""
 
     return redirect_wrapper(render_template('forum.html'))
+
 
 @main_bp.route("/thread/<int:thread_id>")
 def thread_page(thread_id):
@@ -37,8 +42,10 @@ def thread_page(thread_id):
 
     return redirect_wrapper(render_template('thread.html', thread_id=thread_id))
 
+
 @main_bp.route("/profile")
 def profile_page():
     """The profile page"""
     threads = get_posts_from_user(get_user_id_by_token(request.cookies.get('token')))
-    return render_template('profile.html', posts=threads)
+    user = get_user_by_id(get_user_id_by_token(request.cookies.get('token')))
+    return redirect_wrapper(render_template('profile.html', posts=threads, user=user))
