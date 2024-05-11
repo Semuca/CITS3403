27e067c9ca9d5404bc1b050b87051ebdcfd32a9b
sellsol <leowheidi@gmail.com>
2024-05-11 13:45:45 +0800

    Fix for loading profile, page loads but no data is displayed yet

--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,7 @@
 
 from flask import Blueprint, render_template, request
 
-from app.helpers import redirect_wrapper, get_user_id_by_auth_header, get_user_id_by_token, database_manager
-from app.helpers.DatabaseHelper import get_posts_from_user, get_user_by_id
+from app.helpers import redirect_wrapper, get_user_by_auth_header, database_manager
 
 main_bp = Blueprint('main_bp', __name__)
 
@@ -49,6 +48,17 @@ def thread_page(thread_id):
 @main_bp.route("/profile")
 def profile_page():
     """The profile page"""
-    threads = get_posts_from_user(get_user_id_by_token(request.cookies.get('token')))
-    user = get_user_by_id(get_user_id_by_token(request.cookies.get('token')))
+    print(request.headers)
+    user = get_user_by_auth_header()
+
+    # This is here temporarily to resolve merge conflict, will be done with changes to the user model later
+    threads = []
+    if user is not None:
+        from app import db
+        from app.models import ThreadModel
+        threads = db.session.scalars(
+            db.select(ThreadModel)
+            .where(ThreadModel.user_id == user.id)
+            .order_by(ThreadModel.created_at.asc())
+        ).all()
     return redirect_wrapper(render_template('profile.html', posts=threads, user=user))
