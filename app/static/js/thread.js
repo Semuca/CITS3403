e56079f6cb11294421f44db03cf4c0489d774e11
healy <healy_jared@yahoo.com>
2024-05-13 22:13:40 +0800

    Add functional frontend for trade requests

--- app/static/js/thread.js
+++ app/static/js/thread.js
@@ -2,33 +2,82 @@ import {CookieManager} from "./helpers/cookie_manager.js";
 import {dateFromPythonTime, timeFromPythonTime} from "./helpers/format_time.js";
 import { showErrorBanner } from "./helpers/error_banner.js";
 
+const threadId = $("#threadScript").data().threadId;
+
+function acceptTrade(id) {
+    fetch(`/api/threads/${threadId}/offers/${id}`, {
+        method: "POST", headers: {
+            Authorization: `Bearer ${CookieManager.getCookie("token")}`,
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(o => {
+        if (!o.ok){
+            switch (o.status) {
+                case 400: {
+                    alert("You do not have the correct amount of items for this trade!")
+                    break
+                }
+                case 403: {
+                    alert("Only the user who made the thread can accept the trade!")
+                    break
+                }
+                default:
+                    alert(`An unexpected error occured (${o.errorMessage})`)
+            }
+        }
+    })
+}
 
 $(document).ready(() => {
-    const threadId = $("#threadScript").data().threadId;
+
+
     fetch(`/api/threads/${threadId}/children`, {
-        method: "POST", headers: {
+        method: "GET", headers: {
             Authorization: `Bearer ${CookieManager.getCookie("token")}`,
             "Content-type": "application/json; charset=UTF-8"
         }
     }).then(r => {
         if (r.ok) {
             r.json().then(j => {
-                for (const i in j) {
-                   $("#comments").append(`
+                for (const i of j) {
+                    console.log(i)
+                    if (i.childType === "comment") {
+                        $("#comments").append(`
                        <li>
-                           <div className="timeline-time">
-                               <span className="date">${dateFromPythonTime(i.created_at)}</span>
-                               <span className="time">${timeFromPythonTime(i.created_at)}</span>
+                           <div class="timeline-time">
+                               <span class="date">${dateFromPythonTime(i.createdAt)}</span>
+                               <span class="time">${timeFromPythonTime(i.createdAt)}</span>
                            </div>
-                           <div className="timeline-body">
-                               <div className="timeline-content">
-                                   <h5 className="comment-username mb-1">${i.username}</h5>
-                                   <p>{{i.comment_text}}</p>
+                           <div class="timeline-body">
+                               <div class="timeline-content">
+                                   <h5 class="comment-username mb-1">${i.user.username}</h5>
+                                   <p>${i.commentText}</p>
                                </div>
                            </div>
                        </li>`
-                   )
+                        )
+                    } else {
+                        $("#comments").append(`
+                       <li>
+                           <div class="timeline-time">
+                               <span class="date">${dateFromPythonTime(i.createdAt)}</span>
+                               <span class="time">${timeFromPythonTime(i.createdAt)}</span>
+                           </div>
+                           <div class="timeline-body">
+                               <div class="timeline-content">
+                                   <h4>Trade Request from ${i.user.username}</h4>
+                                   <p id="trade${i.id}">${i.offering} for ${i.wanting}</p>
+                                   <button class="trade" id="${i.id}">Accept</button>
+                               </div>
+                           </div>
+                       </li>`)
+                    }
+
                 }
+                $(".trade").on("click", (e) => {
+                    acceptTrade(e.target.id)
+                })
+
             })
         }
     })
@@ -45,7 +94,32 @@ $(document).ready(() => {
         }).then(r => {
             if (r.ok) {
                 r.json().then(o => {
-                    //easier to reload the page and let the server deal with adding a new comment...
+                    location.reload()
+                });
+            } else {
+                alert(`The server did not return a valid response! HTTP error code is ${r.status} (${r.statusText})`)
+            }
+        })
+    });
+
+    $("#newTrade").on("click", () => {
+        let offer = []
+        let want = []
+        for (let i = 0; i < 10; i++) {
+            offer.push(+$(`#give${i}`).val())
+            want.push(+$(`#get${i}`).val())
+        }
+        fetch(`/api/threads/${threadId}/offers`, {
+            method: "POST", headers: {
+                Authorization: `Bearer ${CookieManager.getCookie("token")}`,
+                "Content-type": "application/json; charset=UTF-8"
+            }, body: JSON.stringify({
+                offeringList: offer,
+                wantingList: want
+            })
+        }).then(r => {
+            if (r.ok) {
+                r.json().then(o => {
                     location.reload()
                 });
             } else {
@@ -55,3 +129,4 @@ $(document).ready(() => {
     });
 })
 ;
+
