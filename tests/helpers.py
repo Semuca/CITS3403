cf1107db249bec14c94a8f96e2d11b127c9d42d0
Semuca <pokelock@icloud.com>
2024-05-18 18:15:41 +0800

    Added selenium setup for tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,8 +1,17 @@
+"""Helper functions for tests"""
 
+import multiprocessing
 import unittest
+from urllib.parse import urlparse
+
+from selenium import webdriver
 
 from app import create_app
 from app.databases import db
+
+multiprocessing.set_start_method("fork")
+
+LOCALHOST = "http://localhost:5000"
 
 class BaseApiTest(unittest.TestCase):
     def setUp(self):
@@ -28,3 +37,37 @@ def get_api_headers():
         'Accept': '*/*',
         'Content-Type': 'application/json'
     }
+
+class BaseSeleniumTest(unittest.TestCase):
+    def setUp(self):
+        # create app so db can be linked to it
+        self.app = create_app('test')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
+        # create all tables in the db
+        db.create_all()
+
+        self.server_process = multiprocessing.Process(target=lambda : self.app.run(debug=True, use_reloader=False), daemon=True)
+        self.server_process.start()
+
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("-headless")
+
+        self.driver = webdriver.Chrome()
+        self.driver.get(LOCALHOST)
+
+    def tearDown(self):
+        # stop db session and clear out all data
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+        self.server_process.terminate()
+
+        self.driver.quit()
+
+def getPath(url):
+    return urlparse(url).path
+
---
+++ tests/test_pages_home.py
@@ -0,0 +1,106 @@
+"""System tests for home page"""
+
+import unittest
+
+from app.databases import db
+from app.models import UserModel
+
+from .helpers import BaseSeleniumTest, getPath
+
+class TestBasePage(BaseSeleniumTest):
+    """Tests the base page wrapper"""
+
+    def test_unauthenticated_base(self):
+        """Tests unauthenticated navigation using the base page wrapper"""
+
+        # Navigating to the home page should work successfully
+        link = self.driver.find_element('id', 'home-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/')
+
+        # Navigating to the sign up page should work successfully
+        link = self.driver.find_element('id', 'sign-up-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/register')
+
+        # Navigating to the log in page should work successfully
+        link = self.driver.find_element('id', 'log-in-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/login')
+
+        # Navigating to the forum page should redirect to the login page if unauthenticated
+        link = self.driver.find_element('id', 'forum-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/login')
+
+        # Navigating to the game page should redirect to the login page if unauthenticated
+        link = self.driver.find_element('id', 'game-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/login')
+
+        # Navigating to the profile page should redirect to the login page if unauthenticated
+        link = self.driver.find_element('id', 'profile-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/login')
+
+    def test_authenticated_base(self):
+        """Tests authenticated navigation using the base page wrapper"""
+
+        # Assemble
+        db.session.add(UserModel(
+            username="PurpleGuy",
+            description="I'm the man behind the slaughter",
+            password_hash="Password123",
+            authentication_token="authtest",
+            security_question=3,
+            security_question_answer="idk"
+        ))
+        db.session.commit()
+
+        self.driver.add_cookie({'name': 'token', 'value': 'authtest'})
+
+        # Navigating to the home page should work successfully
+        link = self.driver.find_element('id', 'home-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/')
+
+        # Navigating to the sign up page should work successfully
+        link = self.driver.find_element('id', 'sign-up-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/register')
+
+        # Navigating to the log in page should work successfully
+        link = self.driver.find_element('id', 'log-in-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/login')
+
+        # Navigating to the forum page should work successfully
+        link = self.driver.find_element('id', 'forum-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/forum')
+
+        # Navigating to the game page should work successfully
+        link = self.driver.find_element('id', 'game-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/game')
+
+        # Navigating to the profile page should work successfully
+        link = self.driver.find_element('id', 'profile-nav-link')
+        link.click()
+
+        self.assertEqual(getPath(self.driver.current_url), '/profile')
+
+
+if __name__ == '__main__':
+    unittest.main()
